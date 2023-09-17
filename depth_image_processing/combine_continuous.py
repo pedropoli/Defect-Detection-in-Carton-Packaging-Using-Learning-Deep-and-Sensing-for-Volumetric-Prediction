ef7100c9552d388ee95c91d231caf97e16fff889
Pedro Henrique Poli <54546953+pedropoli@users.noreply.github.com>
2023-09-17 14:55:50 -0300

    Add code from pedropoli

--- depth_image_processing/combine_continuous.py
+++ depth_image_processing/combine_continuous.py
@@ -1,3 +1,5 @@
+# This code has changes from its original version: kinect_3d_dev @janbijster
+
 import transforms3d
 import numpy as np
 import utils
@@ -5,13 +7,13 @@ import time
 import open3d
 import functools
 import colorsys
-import os
+import os, sys
 
 
 # params
-session_name = 'v1test1'
-snapshot_number = 0
-num_sensors = 2
+session_name = 'testvoxel2'
+snapshot_number = 59
+num_sensors = 1
 depth_images_folder = '../data/depth_scans'
 calibration_matrices_folder = '../data/calibration'
 
@@ -28,8 +30,27 @@ transformations = np.load(calibration_matrices_filename)
 
 snapshots_processed = set([])
 voxels_combined = None
-
 all_snapshots = []
+
+# Final Paper Development:
+# Função de voxelização:
+# def pick_points(vis):
+#     pick_event=vis.get_picked_points()
+#     if pick_event is not None:
+#         print("Indices do ponto escolhido:", pick_event)
+#         picked_points = point_cloud.points[pick_event, :]
+#         print("Coordenadas 3D:", picked_points)
+#     return picked_points
+
+def voxelization(point_cloud):
+    voxel_grid=open3d.geometry.VoxelGrid.create_from_point_cloud(point_cloud, voxel_size=0.0009)
+    vis = open3d.visualization.Visualizer()
+    vis.create_window(window_name='Box Visualize', width=800, height=600)
+    vis.add_geometry(voxel_grid)
+    vis.run()
+    return voxel_grid
+
+
 # Main loop:
 while True:
 
@@ -52,10 +73,19 @@ while True:
                     # snapshot is not yet processed
                     process_snapshot = snapshot_index
                     break
-
+        # print(process_snapshot, snapshot_number)
+        # if process_snapshot == snapshot_number:
+        #     done = True
         if process_snapshot == -1:
             print('no new snapshots ready, waiting...')
             time.sleep(1)
+            cmd = input("Press q to quit")
+            if cmd == 'q':
+                sys.exit()
+            if cmd == 'p':
+                for snaps in all_snapshots:
+                    rearranged_pc = voxelization(pointcloud)
+
         else:
             print('new snapshot ready: {}'.format(process_snapshot))
             break
@@ -64,22 +94,32 @@ while True:
     # load depth scans and combine to snapshot model:
     snapshot_pointcloud = None
     for sensor_index in range(num_sensors):
-
-        depth_image_filename = '{}/{}/{:04d}_{:02d}.npy'.format(depth_images_folder, session_name, process_snapshot, sensor_index)
+        depth_image_filename = '{}/{}/{:04d}_{:02d}.npy'.format(depth_images_folder, session_name, process_snapshot,
+                                                                sensor_index)
         print('Opening image {}...'.format(depth_image_filename))
         depth_image = np.load(depth_image_filename)
-        pointcloud = utils.depth_image_to_pointcloud(depth_image, filter=filter)
+        raw_pointcloud = depth_image     # salva pointcloud sem alterações
+        pointcloud = utils.depth_image_to_pointcloud(depth_image, filter=filter)    #
         if pointcloud is None:
             continue
         pointcloud.paint_uniform_color(utils.item_in_range_color(process_snapshot, 10, True))
         transformation = transformations[transformations.files[sensor_index]]
         pointcloud.transform(transformation)
         snapshot_pointcloud = utils.merge_pointclouds(snapshot_pointcloud, pointcloud)
-    
+
     snapshots_processed.add(process_snapshot)
     if snapshot_pointcloud is None:
         continue
 
     # Got a new snapshot pointcloud.
     all_snapshots.append(snapshot_pointcloud)
-    open3d.draw_geometries(all_snapshots)
+    # open3d.visualization.draw_geometries(all_snapshots)
+
+
+    # Voxelization Function: align poin clouds in one 3D image
+
+
+
+# for pointclouds in all_snapshots:
+#     rearranged_pc = voxelization(pointclouds, 19)
+#     open3d.draw_geometries(rearranged_pc)
